447c0e799436085c9044c2ca917715c32105c8cd
drminor <david.minor@live.com>
2017-07-22 01:58:37 -0400

    Test Update

---
+++ PropBag.Test/GTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropBagLib.Tests
+{
+    /// <summary>
+    /// This is just a test for git hub.
+    /// </summary>
+    class GTest
+    {
+        
+    }
+}
