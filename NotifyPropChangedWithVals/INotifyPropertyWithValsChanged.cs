1c1b0da9f4db283c25d5209196cc9c4580876c03
drminor <david.minor@live.com>
2017-07-26 01:45:34 -0400

    Fixed Local Repo

---
+++ NotifyPropChangedWithVals/INotifyPropertyWithValsChanged.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace DRM.Ipnwv
+{
+    /// <summary>
+    /// Notifies clients that a property value has changed and provides the old and new values.
+    /// </summary>
+    public interface INotifyPropertyChangedWithVals
+    {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        event PropertyChangedWithValsHandler PropertyChangedWithVals;
+
+    }
+}
+
---
+++ PropBagT4/RT_Builder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropBagT4
+{
+    public class RT_Builder
+    {
+
+        static public void BuildIt()
+        {
+            RT x = new RT();
+            string output = x.TransformText();
+
+
+        }
+
+
+    }
+}
