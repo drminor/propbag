88d4cb382982f731f0dc798deac27dd00df944be
drminor <david.minor@live.com>
2017-09-23 21:15:43 -0400

    Added PropBagMapperCustom (Uses AutoMapper ExtraMembers)

---
+++ AutoMapperSupport/PropBagMapperCustom.cs
@@ -0,0 +1,278 @@
+﻿using AutoMapper;
+using DRM.PropBag.ControlModel;
+using DRM.WrapperGenLib;
+using System;
+using System.Collections.Generic;
+
+using DRM.PropBag.LiveClassGenerator;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.IO;
+using DRM.PropBag.ControlsWPF;
+using DRM.TypeSafePropertyBag;
+using DRM.PropBag;
+using System.Linq;
+using AutoMapper.ExtraMembers;
+using System.Linq.Expressions;
+
+namespace DRM.AutoMapperSupport
+{
+    public class PropBagMapperCustom<RegT, PbT> : IPropBagMapperGen where PbT : IPropBag, new()
+    {
+        #region Public and Private Members
+
+
+        PropModel _propModel;
+        //Dictionary<string, Type> _propDefs;
+
+        IEnumerable<MemberInfo> _extraMembers = new List<MemberInfo>();
+
+        Func<object, object> _regularInstanceCreator;
+
+        public IMapper Mapper { get; set; }
+
+        TypePair _typePair;
+        public TypePair TypePair => _typePair;
+
+        #endregion
+
+        #region Constructors
+
+        public PropBagMapperCustom(string newTypeName, Dictionary<string, Type> propDefs) : this(newTypeName, propDefs, null) { }
+
+        public PropBagMapperCustom(string newTypeName, Dictionary<string, Type> propDefs, Func<object, object> regularInstanceCreator)
+        {
+            _propModel = null;
+
+            //PbT destination = GetNewDestination();
+
+            //IPropBag ip = (IPropBag)destination;
+            //IEnumerable<MemberInfo> extraMembers = ip.BuildPropertyInfoList<PbT>();
+
+            _extraMembers = GetExtraMembers(propDefs);
+
+            _regularInstanceCreator = regularInstanceCreator;
+
+            _typePair = new TypePair(typeof(RegT), typeof(PbT));
+        }
+
+        public PropBagMapperCustom(BoundPropBag boundPB) : this(boundPB, null, null) { }
+
+        public PropBagMapperCustom(BoundPropBag boundPB, IModuleBuilderInfo moduleBuilderInfo) : this(boundPB, moduleBuilderInfo, null) { }
+
+        public PropBagMapperCustom(BoundPropBag boundPB, IModuleBuilderInfo moduleBuilderInfo, Func<object, object> regularInstanceCreator)
+        {
+            _propModel = boundPB.PropModel;
+
+            Dictionary<string, Type> propDefs = boundPB.PropModel.GetPropertyDefs();
+
+            _extraMembers = GetExtraMembers(propDefs);
+
+            _regularInstanceCreator = regularInstanceCreator;
+
+            _typePair = new TypePair(typeof(RegT), typeof(PbT));
+        }
+
+        #endregion
+
+        #region Construction Support
+
+        private List<MemberInfo> GetExtraMembers(IDictionary<string, Type> props)
+        {
+            List<MemberInfo> result = new List<MemberInfo>();
+
+            foreach (KeyValuePair<string, Type> kvp in props)
+            {
+                string propertyName = kvp.Key;
+                Type propertyType = kvp.Value;
+
+                Func<object, string, Type, object> getter =
+                    new Func<object, string, Type, object>((host, pn, pt) => ((PbT)host).GetValWithType(pn, pt));
+
+                Action<object, string, Type, object> setter =
+                    new Action<object, string, Type, object>((host, pn, pt, value) => ((PbT)host).SetValWithType(pn, pt, value));
+
+                PropertyInfoWT pi = new PropertyInfoWT(propertyName, propertyType, typeof(PbT),
+                    getter, setter);
+
+                result.Add(pi);
+
+            }
+
+            return result;
+        }
+
+        public IMapperConfigurationExpression Configure(IMapperConfigurationExpression cfg)
+        {
+            ConfigIt(cfg, _extraMembers, _regularInstanceCreator);
+            return cfg;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T">The base type to use, must Implement ITypeSafePropBag</typeparam>
+        /// <param name="newTypeName"></param>
+        /// <param name="regularType"></param>
+        /// <param name="propDefs"></param>
+        /// <returns></returns>
+        public void ConfigIt(IMapperConfigurationExpression cfg, IEnumerable<MemberInfo> extraMembers, Func<object, object> regularInstanceCreator) 
+        {
+
+            cfg.ShouldMapField = ShouldMap;
+            cfg.ShouldMapProperty = ShouldMap;
+
+            //cfg.IncludeExtraMembersForType(typeof(Destination), extraMembers);
+
+            //if(null != cfg.GetExtraGetterStrategy(PropertyInfoWT.STRATEGEY_KEY))
+            //{
+            //    cfg.DefineExtraMemberGetterBuilder(PropertyInfoWT.STRATEGEY_KEY, GetGetterStrategy);
+            //}
+
+            //if (null != cfg.GetExtraSetterSrategry(PropertyInfoWT.STRATEGEY_KEY))
+            //{
+            //    cfg.DefineExtraMemberSetterBuilder(PropertyInfoWT.STRATEGEY_KEY, GetSetterStrategy);
+            //}
+
+
+            cfg
+                .CreateMap<RegT, PbT>()
+                //.RegisterExtraMembers(cfg)
+                .AddExtraDestintionMembers(extraMembers)
+                //.ForMember("Item1", opt => opt.Condition(srcA => srcA.Item1 != "AA"))
+            ;
+
+            //Func<Destination, bool> cond = (s => "x" != (string)s.GetIt("Item1", typeof(string)));
+
+            cfg
+                .CreateMap<PbT, RegT>()
+                .AddExtraSourceMembers(extraMembers)
+                //.ForMember("Item1", opt => opt.Condition(cond))
+            ;
+
+        }
+
+        #endregion
+
+        #region Extra Member Support
+
+
+
+        public bool ShouldMap(MemberInfo mi)
+        {
+            if(IsPublic(mi)) return true;
+
+            Attribute[] atts = mi.GetCustomAttributes(true) as Attribute[];
+            if (atts == null) return false;
+            Attribute test = atts.FirstOrDefault(a => a is ExtraMemberAttribute);
+
+            return test != null;
+        }
+
+        private bool IsPublic(MemberInfo mi)
+        {
+            if (mi is MethodInfo methInfo) return methInfo.IsPublic;
+            if (mi is PropertyInfo pi) return pi.GetMethod.IsPublic;
+            return false;
+        }
+
+        #endregion
+
+        #region IPropBagMapperGen implementation
+
+        //public delegate object MapFromX(IPropBag source);
+        //public delegate IPropBag MapToX(object source, IPropBag destination);
+        //public delegate IPropBag MapToNewX(object source);
+
+        //MapFromX MapFrom
+        //{
+        //    get
+        //    {
+        //        return MapFromX;
+        //    }
+        //}
+
+        //MapToX MapTo
+        //{
+        //    get
+        //    {
+        //        return MapToX;
+        //    }
+        //}
+
+        //public MapToNewX MapToNew
+        //{
+        //    get
+        //    {
+        //        return MapToNewX;
+        //    }
+        //}
+
+        #endregion
+
+        #region Type Mapper Function
+
+        //public object MapFromX(IPropBag source)
+        //{
+        //    return (RegT)MapFrom((PbT)source);
+        //}
+
+        //public IPropBag MapToX(object source, IPropBag destination)
+        //{
+        //    return MapTo((RegT)source, (PbT)destination);
+        //}
+
+        //public IPropBag MapToNewX(object source)
+        //{
+        //    return (PbT)MapTo((RegT)source);
+        //}
+
+        public RegT MapFrom(PbT source)
+        {
+
+            return (RegT)Mapper.Map<PbT, RegT>(source);
+        }
+
+        public PbT MapTo(RegT source, PbT destination)
+        {
+            PbT result = Mapper.Map<RegT, PbT>(source, destination);
+            return result;
+        }
+
+        public PbT MapTo(RegT source)
+        {
+            if (_propModel == null) throw new InvalidOperationException($"The version of MapTo that doesn't take a {typeof(PbT)} is not supported. This instance was not created from a BoundPropBagTemplate.");
+
+            PbT destination = GetNewDestination();
+            return MapTo(source, destination);
+        }
+
+        private PbT GetNewDestination()
+        {
+            PbT destination;
+            try
+            {
+                destination = (PbT)Activator.CreateInstance(typeof(PbT), new object[] { _propModel });
+                return destination;
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Cannot create an instance of {typeof(PbT)} that takes a PropModel parameter.");
+            }
+        }
+
+        #endregion
+
+        #region DevelopementWork Get Key Pair
+
+        //private static void GetKeys()
+        //{
+        //    FileStream fs = new FileStream(@"C:\Users\david_000\Source\keyPair.snk", FileMode.Open);
+        //    StrongNameKeyPair kp = new StrongNameKeyPair(fs);
+        //    fs.Close();
+        //}
+
+        #endregion
+
+    }
+}
---
+++ AutoMapperSupport/PropertyInfoWT.cs
@@ -0,0 +1,224 @@
+﻿using AutoMapper.ExtraMembers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRM.AutoMapperSupport
+{
+    /// <summary>
+    /// This implementation requires that the Getter and Setter MethodsInfo provided will be called
+    /// on an instance (instead of being static methods).
+    /// 
+    /// The Getter must have a signature of Func&lt;object, Type, object> where the first parameter reference the instance on which the 
+    /// method will be called, the second parameter is the type of an object which is intended to receive the value produced 
+    /// from the getter. The getter MethodInfo's GetValue methods will return an object of the type specified by the property defintion
+    /// and will optionally perform type checks using the Type provided by the caller (in the second parameter.)
+    /// 
+    /// The Setter must have a signature of Action&lt;object, type, object> where the first parameter references the instance on which the 
+    /// method will be called, the second paramter is the type of an object which is intended to be supplied as the value of the set calls,
+    /// and the third parameter is the value that will be assigned to the property. The setter will optionally perform tyep checks 
+    /// usig the Type provided by the caller (in the second parameter.)
+    /// 
+    /// If the getter is called without a type parameter, the type of the property will be supplied to the Getter Method.
+    /// Likewise for the setter.
+    /// 
+    /// There are two constructors, one that takes a pair of MethodInfos, and one that takes a pair of delegates with the same signatures as described above.
+    /// If MethodInfos are used, an instance of IHaveExtraMembers must be provided on which the MethodInfos will be called.
+    /// If the MethodInfos reference static methods, then this "bridge" object should be set to null.
+    ///  
+    /// No such target is needed for the pair of delegates, since they incorporate the target in their definition.
+    /// If anonmous delegates are used, these will be private, static methods. AutoMapper defaults to only considering
+    /// source members that are public, instance methods therefor in order for this to work you must supply a custom 
+    /// ShouldMapProperty and ShouldMapField functions.
+    /// </summary>
+    public class PropertyInfoWT : PropertyInfo
+    {
+        public const string STRATEGEY_KEY = "WithType";
+
+        Type _hostType;
+        //IHaveExtraMembers _bridge;
+        //MethodInfo _getter;
+        //MethodInfo _setter;
+        Func<object, string, Type, object> _getterFunc;
+        Action<object, string, Type, object> _setterAction;
+        bool _useMethodInfos;
+        IEnumerable<Attribute> _attributes;
+
+        public PropertyInfoWT(string name, Type propertyType, Type hostType,
+            Func<object, string, Type, object> getterFunc,
+            Action<object, string, Type, object> setterAction,
+            IEnumerable<Attribute> attributes = null)
+        {
+            Name = name;
+            PropertyType = propertyType;
+            _hostType = hostType;
+            //_bridge = null;
+            _getterFunc = getterFunc;
+            _setterAction = setterAction;
+            _useMethodInfos = false;
+            _attributes = attributes;
+            if (attributes == null)
+            {
+                // TODO: Allow caller to provide list of attributes.
+            }
+        }
+
+        //public PropertyInfoWT(string name, Type propertyType, Type hostType,
+        //    IHaveExtraMembers bridge,
+        //    MethodInfo getter,
+        //    MethodInfo setter,
+        //    IEnumerable<Attribute> attributes = null)
+        //{
+        //    if (bridge == null)
+        //        if (getter.IsStatic || setter.IsStatic || getter.IsPrivate || setter.IsPrivate) throw new ArgumentException("Both the getter and setter MethodInfo arguments must refer to a public static method.");
+        //        else
+        //        if (getter.IsStatic || setter.IsStatic || getter.IsPrivate || setter.IsPrivate) throw new ArgumentException("Both the getter and setter MethodInfo arguments must refer to a public instance method.");
+
+        //    Name = name;
+        //    PropertyType = propertyType;
+        //    _hostType = hostType;
+        //    _bridge = bridge;
+        //    _getter = getter;
+        //    _setter = setter;
+        //    _useMethodInfos = true;
+        //    _attributes = attributes;
+        //}
+
+        public override Type PropertyType { get; }
+
+        public override PropertyAttributes Attributes => PropertyAttributes.None;
+
+        public override bool CanRead => true;
+
+        public override bool CanWrite => true;
+
+        public override string Name { get; }
+
+        public override Type DeclaringType => _hostType;
+
+        public override Type ReflectedType => _hostType;
+
+        public override MethodInfo[] GetAccessors(bool nonPublic)
+        {
+            // We have no private accessors, so base.GetAcessors is equivalent regardless of the value of the nonPublic argument.
+            return base.GetAccessors();
+        }
+
+        public override object[] GetCustomAttributes(bool inherit)
+        {
+            return new Attribute[] { new ExtraMemberAttribute("Test", STRATEGEY_KEY) };
+        }
+
+        public T GetCustomAttribute<T>() where T : System.Attribute
+        {
+            if (typeof(T) == typeof(ExtraMemberAttribute))
+            {
+                return new ExtraMemberAttribute("Default, System Provided Attribute", STRATEGEY_KEY) as T;
+            }
+            return null;
+        }
+
+        public override object[] GetCustomAttributes(Type attributeType, bool inherit)
+        {
+            if (attributeType == typeof(ExtraMemberAttribute))
+            {
+                return new Attribute[] { new ExtraMemberAttribute("Default, System Provided Attribute", STRATEGEY_KEY) };
+            }
+            return new Attribute[0];
+        }
+
+        // TODO: Make this a lazy singleton
+        public override MethodInfo GetGetMethod(bool nonPublic)
+        {
+            if (_useMethodInfos)
+            {
+                return null;
+                //return _getter;
+                ////Func<object, object> temp = new Func<object, object>((host) => GetValue(host, null));
+                ////return temp.Method; // Getter.Method; 
+            }
+            else
+            {
+                return _getterFunc.Method;
+            }
+        }
+
+        public override ParameterInfo[] GetIndexParameters()
+        {
+            return new ParameterInfo[0];
+        }
+
+        // TODO: Make this a lazy singleton
+        public override MethodInfo GetSetMethod(bool nonPublic)
+        {
+            if (_useMethodInfos)
+            {
+                return null;
+                ////Action<object, object> temp = new Action<object, object>((host, value) => SetValue(host, value, null));
+                ////return temp.Method; // Setter.Method;
+                //return _setter;
+            }
+            else
+            {
+                return _setterAction.Method;
+            }
+
+        }
+        public override object GetValue(object obj, BindingFlags invokeAttr, Binder binder, object[] index, CultureInfo culture)
+        {
+            Type pType;
+            if (index != null && index.Length > 0)
+            {
+                pType = (Type)index[0];
+            }
+            else
+            {
+                pType = PropertyType;
+            }
+
+            if (_useMethodInfos)
+            {
+                //object[] parameters = new object[3] { obj, Name, pType };
+                //return _getter.Invoke(_bridge, invokeAttr, binder, parameters, culture);
+                return null;
+            }
+            else
+            {
+                return _getterFunc(obj, Name, pType);
+            }
+        }
+
+        public override bool IsDefined(Type attributeType, bool inherit)
+        {
+            return attributeType == typeof(ExtraMemberAttribute);
+        }
+
+        public override void SetValue(object obj, object value, BindingFlags invokeAttr, Binder binder, object[] index, CultureInfo culture)
+        {
+            Type pType;
+            if (index != null && index.Length > 0)
+            {
+                pType = (Type)index[0];
+            }
+            else
+            {
+                pType = PropertyType;
+            }
+
+            if (_useMethodInfos)
+            {
+                //object[] parameters = new object[4] { obj, Name, pType, value };
+                //_setter.Invoke(_bridge, invokeAttr, binder, parameters, culture);
+            }
+            else
+            {
+                _setterAction(obj, Name, pType, value);
+            }
+        }
+
+    }
+}
