ca1508bda4a987449f28d3dc28d66bffc1f40542
drminor <david.minor@live.com>
2017-09-28 04:41:07 -0400

    AutoMapper now works for EmitProxxy and ExtraMembers.

---
+++ AutoMapperSupport/IMapperConfiBuilderProvider.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using System;
+
+namespace DRM.PropBag.AutoMapperSupport
+{
+    public interface IMapperConfiBuilderProvider
+    {
+        Action<MapperConfigurationExpression> ResetMemberConfigAction { get; }
+
+        Func<MapperConfigurationExpression, MapperConfiguration> BaseConfigBuilder { get; }
+    }
+}
