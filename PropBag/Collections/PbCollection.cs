7867d96be03c0f6f45ade5d7a47d450419135042
drminor <david.minor@live.com>
2017-10-22 03:59:49 -0400

    Added support for IListSource.

---
+++ PropBag/Collections/PbCollection.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections;
+using DRM.PropBag.ControlModel;
+using DRM.TypeSafePropertyBag;
+using DRM.PropBag.TypeDescriptorExtensions;
+using System.Collections.Specialized;
+
+namespace DRM.PropBag.Collections
+{
+    public class PbCollection<T> : ObservableCollection<T>,  ITypedList
+    {
+        #region Private Members
+
+        private PropModel _propModel;
+        private IPropFactory _propFactory;
+
+        IMyTypeDescriptor _pbTypeDesc;
+        IMyTypeDescriptor PbTypeDesc
+        {
+            get
+            {
+                if(_pbTypeDesc == null)
+                {
+                    _pbTypeDesc = new PropBagTypeDescriptor<PropBag>(_propModel, _propFactory, "testing");
+                }
+                return _pbTypeDesc;
+            }
+            set
+            {
+                _pbTypeDesc = value;
+            }
+        }
+
+        #endregion
+
+
+        //public override event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        //protected override event PropertyChangedEventHandler PropertyChanged;
+
+
+        #region Constructors
+
+        private PbCollection() { } // Disallow parameterless constructor
+
+        public PbCollection(PropModel propModel, IPropFactory propFactory, string listName)
+        {
+            init(propModel, propFactory, listName);
+        }
+
+        public PbCollection(PropModel propModel, IPropFactory propFactory, string listName, List<T> list) : base(list)
+        {
+            init(propModel, propFactory, listName);
+        }
+
+        public PbCollection(PropModel propModel, IPropFactory propFactory, string listName, IEnumerable<T> collection) : base(collection)
+        {
+            init(propModel, propFactory, listName);
+        }
+
+        private void init(PropModel propModel, IPropFactory propFactory, string listName)
+        {
+            PbTypeDesc = null;
+            _propModel = propModel ?? throw new ArgumentNullException(nameof(propModel));
+            _propFactory = propFactory ?? throw new ArgumentNullException(nameof(propFactory));
+            ListName = listName ?? throw new ArgumentNullException(nameof(listName));
+
+            //base.CollectionChanged += PbCollection_CollectionChanged;
+        }
+
+        //private void PbCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        //{
+        //    OurOnCollectionChanged(e);
+        //}
+
+        //private void OurOnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        //{
+        //    if(this.CollectionChanged != null)
+        //    {
+        //        this.CollectionChanged(this, e);
+        //    }
+        //}
+
+        #endregion
+
+        public string ListName { get; private set; }
+
+        #region ITypedList members
+
+        PropertyDescriptorCollection ITypedList.GetItemProperties(PropertyDescriptor[] listAccessors)
+        {
+            if (listAccessors != null) throw new NotSupportedException("PbCollection does not support the use of listAccessors.");
+
+            PropertyDescriptorCollection propDescriptors = PbTypeDesc.GetChildProperties();
+            return propDescriptors;
+        }
+
+        string ITypedList.GetListName(PropertyDescriptor[] listAccessors)
+        {
+            if (listAccessors != null) throw new NotSupportedException("PbCollection does not support the use of listAccessors.");
+
+            return ListName;
+        }
+
+        #endregion
+    }
+}
---
+++ PropBag/Collections/PbListSource.cs
@@ -0,0 +1,79 @@
+﻿using DRM.TypeSafePropertyBag;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace DRM.PropBag.Collections
+{
+    public class PbListSource<T> : IMyListSource<T> where T: class
+    {
+        //IPropBag _propBag;
+        //string _propertyName;
+
+        public PbListSource(Func<ObservableCollection<T>> observableCollectionGetter)
+        {
+            UseObservable = true;
+            ObservableCollectionGetter = observableCollectionGetter ?? throw new ArgumentNullException(nameof(observableCollectionGetter));
+        }
+
+        public PbListSource(Func<IEnumerable<T>> enumerableGetter)
+        {
+            UseObservable = false;
+            IEnumerableGetter = enumerableGetter ?? throw new ArgumentNullException(nameof(enumerableGetter));
+        }
+
+        #region Constructor
+
+
+
+        //public PbListSource(IPropBag propBag, string propertyName)
+        //{
+        //    _propBag = propBag ?? throw new ArgumentNullException(nameof(propBag));
+        //    _propertyName = propertyName ?? throw new ArgumentNullException(nameof(propertyName));
+        //}
+
+        #endregion
+
+        public Func<ObservableCollection<T>> ObservableCollectionGetter { get; }
+
+        public Func<IEnumerable<T>> IEnumerableGetter { get;  }
+
+        public bool UseObservable { get;  }
+
+        public Action<IPropBag, string> PersistList { get; }
+
+
+        private ObservableCollection<T> _theList;
+        public ObservableCollection<T> TheList
+        {
+            get
+            {
+                if (_theList == null)
+                {
+                    if (UseObservable)
+                    {
+                        _theList = ObservableCollectionGetter();
+                    }
+                    else
+                    {
+                        _theList = new ObservableCollection<T>(IEnumerableGetter());
+                    }
+                }
+                return _theList;
+            }
+        }
+
+        public IList GetList()
+        {
+            return TheList;
+            //IProp<T> prop = _propBag.GetTypedProp<T>(_propertyName);
+            //T val = prop.TypedValue;
+            //return  val;
+        }
+
+        public bool ContainsListCollection => false;
+    }
+}
