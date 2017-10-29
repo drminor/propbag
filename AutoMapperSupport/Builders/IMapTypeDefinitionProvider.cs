c2e4be254da28d8fac0b12b4d3edf583a90d9124
drminor <david.minor@live.com>
2017-10-29 02:30:43 -0400

    Added new interface: IMapTypeDefinitionProvider and Simple implementation.

---
+++ AutoMapperSupport/Builders/IMapTypeDefinitionProvider.cs
@@ -0,0 +1,10 @@
+﻿using DRM.PropBag.ControlModel;
+using System;
+
+namespace DRM.PropBag.AutoMapperSupport
+{
+    public interface IMapTypeDefinitionProvider
+    {
+        IMapTypeDefinition<T> GetTypeDescription<T>(PropModel propModel, Type typeToWrap, string className);
+    }
+}
---
+++ AutoMapperSupport/Builders/SimpleMapTypeDefinitionProvider.cs
@@ -0,0 +1,21 @@
+﻿using DRM.PropBag.ControlModel;
+using DRM.TypeSafePropertyBag.Fundamentals;
+using System;
+
+namespace DRM.PropBag.AutoMapperSupport
+{
+    public class SimpleMapTypeDefinitionProvider : IMapTypeDefinitionProvider
+    {
+        public IMapTypeDefinition<T> GetTypeDescription<T>(PropModel propModel, Type typeToWrap, string className)
+        {
+            if (typeof(T).IsPropBagBased())
+            {
+                return new MapTypeDefinition<T>(propModel, typeToWrap);
+            }
+            else
+            {
+                return new MapTypeDefinition<T>();
+            }
+        }
+    }
+}
