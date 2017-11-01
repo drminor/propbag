b2b6e7e0f71408c7ea6b0032714dfbee8d67a1ef
drminor <david.minor@live.com>
2017-11-01 01:38:43 -0400

    Documented the properties for interface: IMapTypeDefinition

--- AutoMapperSupport/MapTypeDefinition/IMapTypeDefinition.cs
+++ AutoMapperSupport/MapTypeDefinition/IMapTypeDefinition.cs
@@ -4,19 +4,43 @@ using System;
 
 namespace DRM.PropBag.AutoMapperSupport
 {
+    /// <summary>
+    /// This is the typed version of the IMapTypeDefinitionGen interface.
+    /// It allows one to create a MapTypeDefinition using a type known at compile time.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IMapTypeDefinition<T> : IMapTypeDefinitionGen
     {
     }
 
-    // ToDo: Document these members.
     public interface IMapTypeDefinitionGen : IEquatable<IMapTypeDefinitionGen>
     {
+        /// <summary>
+        /// The type of object that will serve as the source or destination in
+        /// Mapping operations.
+        /// </summary>
         Type Type { get; }
 
+        /// <summary>
+        /// True if Type (the above Type property) implements the IPropBag interface.
+        /// </summary>
         bool IsPropBag { get; }
+
+        /// <summary>
+        /// The PropModel to use when constructing new instances of objects of Type: Type or of Type: NewWrapperType.
+        /// </summary>
         PropModel PropModel { get; }
-        Type NewWrapperType { get; }
+
+        // TODO: Do we need to keep this and keep the PropFactory property of PropModel.
+        /// <summary>
+        /// The PropFactory to use when constructing new instances.
+        /// </summary>
         IPropFactory PropFactory { get; }
 
+        /// <summary>
+        /// When a Proxy or Wrapper Type must be created to support the mapping operation, this holds the
+        /// new emitted type.
+        /// </summary>
+        Type NewWrapperType { get; }
     }
 }
