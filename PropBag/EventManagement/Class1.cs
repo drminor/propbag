bee019ac3ac9128e96111de19080dc093dccb5df
drminor <david.minor@live.com>
2017-11-05 01:43:35 -0500

    Added new class: PropertyChangedEventManager. It not being used at this time.

---
+++ PropBag/EventManagement/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRM.PropBag
+{
+    class Class1
+    {
+        INotifyPropertyChanged r;
+        System.Windows.IWeakEventListener f;
+        PropertyChangedEventManager pcem;
+    }
+}
