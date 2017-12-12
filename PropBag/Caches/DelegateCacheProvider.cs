534bb147468d2be171e94a8766c73f7a832d81d3
drminor <david.minor@live.com>
2017-12-12 18:30:30 -0500

    Moved the CallPcEventSubscriber delegate cache from PubPropBag to TypeSafePropertyBag project.

--- PropBag/Caches/DelegateCacheProvider.cs
+++ PropBag/Caches/DelegateCacheProvider.cs
@@ -19,16 +19,6 @@ namespace DRM.PropBag.Caches
 
         Lazy<DelegateCache<DoSetDelegate>> _doSetCache;
 
-        Lazy<DelegateCache<CallPcObjEventSubscriberDelegate>> _callPcObjEventSubsCache;
-
-        Lazy<DelegateCache<CallPcGenEventSubscriberDelegate>> _callPcGenEventSubsCache;
-
-        Lazy<DelegateCache<CallPcTypedEventSubscriberDelegate>> _callPcTypeEventSubsCache;
-
-        Lazy<DelegateCache<CallPcStandardEventSubscriberDelegate>> _callPcStEventSubsCache;
-
-        Lazy<DelegateCache<CallPChangingEventSubscriberDelegate>> _callPChangingEventSubsCache;
-
 
         Lazy<DelegateCache<CreatePropFromStringDelegate>> _createPropFromStringCache;
 
@@ -44,17 +34,6 @@ namespace DRM.PropBag.Caches
         public TypeDescBasedTConverterCache TypeDescBasedTConverterCache => _converterCache.Value;
 
         internal DelegateCache<DoSetDelegate> DoSetDelegateCache => _doSetCache.Value;
-
-        internal DelegateCache<CallPcObjEventSubscriberDelegate> CallPcObjEventSubsCache => _callPcObjEventSubsCache.Value;
-
-        internal DelegateCache<CallPcGenEventSubscriberDelegate> CallPcGenEventSubsCache => _callPcGenEventSubsCache.Value;
-
-        internal DelegateCache<CallPcTypedEventSubscriberDelegate> CallPcTypedEventSubsCache => _callPcTypeEventSubsCache.Value;
-
-        internal DelegateCache<CallPcStandardEventSubscriberDelegate> CallPcStEventSubsCache => _callPcStEventSubsCache.Value;
-
-        internal DelegateCache<CallPChangingEventSubscriberDelegate> CallPChangingEventSubsCache => _callPChangingEventSubsCache.Value;
-        
         
 
         internal DelegateCache<CreatePropFromStringDelegate> CreatePropFromStringCache => _createPropFromStringCache.Value;
@@ -66,7 +45,7 @@ namespace DRM.PropBag.Caches
 
         #endregion
 
-        #region The Static Constructor
+        #region Constructor
 
         public DelegateCacheProvider()
         {
@@ -81,85 +60,36 @@ namespace DRM.PropBag.Caches
                     () => new DelegateCache<DoSetDelegate>(doSetMethodInfo), LazyThreadSafetyMode.PublicationOnly
                 );
 
-
-            #region Raise Event Delegates
-
-            // PcObject
-            MethodInfo callPcObjEventSubscriber_mi = typeof(PropBag).GetMethod("CallPcObjectEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
-
-            _callPcObjEventSubsCache =
-                new Lazy<DelegateCache<CallPcObjEventSubscriberDelegate>>
-                (
-                    () => new DelegateCache<CallPcObjEventSubscriberDelegate>(callPcObjEventSubscriber_mi), LazyThreadSafetyMode.PublicationOnly
-                );
-
-            // PcGen
-            MethodInfo callPcGenEventSubscriber_mi = typeof(PropBag).GetMethod("CallPcGenEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
-
-            _callPcGenEventSubsCache =
-                new Lazy<DelegateCache<CallPcGenEventSubscriberDelegate>>
-                (
-                    () => new DelegateCache<CallPcGenEventSubscriberDelegate>(callPcGenEventSubscriber_mi), LazyThreadSafetyMode.PublicationOnly
-                );
-
-            // PcTyped
-            MethodInfo callPcTypedEventSubscriber_mi = typeof(PropBag).GetMethod("CallPcTypedEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
-
-            _callPcTypeEventSubsCache =
-                new Lazy<DelegateCache<CallPcTypedEventSubscriberDelegate>>
-                (
-                    () => new DelegateCache<CallPcTypedEventSubscriberDelegate>(callPcTypedEventSubscriber_mi), LazyThreadSafetyMode.PublicationOnly
-                );
-
-            // PcStandard
-            MethodInfo callPcStEventSubscriber_mi = typeof(PropBag).GetMethod("CallPcStandardEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
-
-            _callPcStEventSubsCache =
-                new Lazy<DelegateCache<CallPcStandardEventSubscriberDelegate>>
-                (
-                    () => new DelegateCache<CallPcStandardEventSubscriberDelegate>(callPcStEventSubscriber_mi), LazyThreadSafetyMode.PublicationOnly
-                );
-
-            // PcChanging
-            MethodInfo callPChangingEventSubscriber_mi = typeof(PropBag).GetMethod("CallPChaningEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
-
-            _callPChangingEventSubsCache =
-                new Lazy<DelegateCache<CallPChangingEventSubscriberDelegate>>
-                (
-                    () => new DelegateCache<CallPChangingEventSubscriberDelegate>(callPChangingEventSubscriber_mi), LazyThreadSafetyMode.PublicationOnly
-                );
-
             #endregion
 
-            #region Prop / CProp / DTProp Creation
+        #region Prop / CProp / DTProp Creation
 
-            // Create Prop From String
-            MethodInfo createPropNoVal_mi = typeof(APFGenericMethodTemplates).GetMethod("CreatePropWithNoValue", BindingFlags.Static | BindingFlags.NonPublic);
-            _createPropWithNoValCache =
-                new Lazy<DelegateCache<CreatePropWithNoValueDelegate>>
-                (
-                    () => new DelegateCache<CreatePropWithNoValueDelegate>(createPropNoVal_mi), LazyThreadSafetyMode.PublicationOnly
-                );
+        // Create Prop From String
+        MethodInfo createPropNoVal_mi = typeof(APFGenericMethodTemplates).GetMethod("CreatePropWithNoValue", BindingFlags.Static | BindingFlags.NonPublic);
+        _createPropWithNoValCache =
+            new Lazy<DelegateCache<CreatePropWithNoValueDelegate>>
+            (
+                () => new DelegateCache<CreatePropWithNoValueDelegate>(createPropNoVal_mi), LazyThreadSafetyMode.PublicationOnly
+            );
 
-            // Create Prop With No Value
-            MethodInfo createPropFromString_mi = typeof(APFGenericMethodTemplates).GetMethod("CreatePropFromString", BindingFlags.Static | BindingFlags.NonPublic);
-            _createPropFromStringCache =
-                new Lazy<DelegateCache<CreatePropFromStringDelegate>>
-                (
-                    () => new DelegateCache<CreatePropFromStringDelegate>(createPropFromString_mi), LazyThreadSafetyMode.PublicationOnly
-                );
+        // Create Prop With No Value
+        MethodInfo createPropFromString_mi = typeof(APFGenericMethodTemplates).GetMethod("CreatePropFromString", BindingFlags.Static | BindingFlags.NonPublic);
+        _createPropFromStringCache =
+            new Lazy<DelegateCache<CreatePropFromStringDelegate>>
+            (
+                () => new DelegateCache<CreatePropFromStringDelegate>(createPropFromString_mi), LazyThreadSafetyMode.PublicationOnly
+            );
 
-            // TODO: This is not being used.
-            // Create Prop From Object
-            MethodInfo createPropFromObject_mi = typeof(APFGenericMethodTemplates).GetMethod("CreatePropFromObject", BindingFlags.Static | BindingFlags.NonPublic);
-            _createPropFromObjectCache =
-                new Lazy<DelegateCache<CreatePropFromObjectDelegate>>
-                (
-                    () => new DelegateCache<CreatePropFromObjectDelegate>(createPropFromObject_mi), LazyThreadSafetyMode.PublicationOnly
-                );
-
-            #endregion
+        // TODO: This is not being used.
+        // Create Prop From Object
+        MethodInfo createPropFromObject_mi = typeof(APFGenericMethodTemplates).GetMethod("CreatePropFromObject", BindingFlags.Static | BindingFlags.NonPublic);
+        _createPropFromObjectCache =
+            new Lazy<DelegateCache<CreatePropFromObjectDelegate>>
+            (
+                () => new DelegateCache<CreatePropFromObjectDelegate>(createPropFromObject_mi), LazyThreadSafetyMode.PublicationOnly
+            );
         }
+
         #endregion
     }
 }
---
+++ TypeSafePropertyBag/Subscriptions/SimpleHandlerDispatchDelegateCacheProvider.cs
@@ -0,0 +1,54 @@
+﻿using DRM.TypeSafePropertyBag.Fundamentals;
+using System;
+using System.Reflection;
+
+namespace DRM.TypeSafePropertyBag
+{
+    using BridgeType = HandlerDispatchGenericBridges;
+
+    public class SimpleHandlerDispatchDelegateCacheProvider : IProvideHandlerDispatchDelegateCaches
+    {
+        #region Public Properties
+
+        public TwoTypesDelegateCache<CallPcTypedEventSubscriberDelegate> CallPcTypedEventSubsCache { get; }
+
+        public DelegateCache<CallPcObjEventSubscriberDelegate> CallPcObjEventSubsCache { get; }
+
+        public DelegateCache<CallPcGenEventSubscriberDelegate> CallPcGenEventSubsCache { get; }
+
+        public DelegateCache<CallPcStandardEventSubscriberDelegate> CallPcStEventSubsCache { get; }
+
+        public DelegateCache<CallPChangingEventSubscriberDelegate> CallPChangingEventSubsCache { get; }
+
+        public DelegateProxyCache DelegateProxyCache { get; }
+
+        #endregion
+
+        public SimpleHandlerDispatchDelegateCacheProvider()
+        {
+            Type bridgeType = typeof(BridgeType);
+
+            // PcTyped
+            MethodInfo callPcTypedEventSubscriber_mi = bridgeType.GetMethod("CallPcTypedEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
+            CallPcTypedEventSubsCache = new TwoTypesDelegateCache<CallPcTypedEventSubscriberDelegate>(callPcTypedEventSubscriber_mi);
+
+            // PcGen
+            MethodInfo callPcGenEventSubscriber_mi = bridgeType.GetMethod("CallPcGenEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
+            CallPcGenEventSubsCache = new DelegateCache<CallPcGenEventSubscriberDelegate>(callPcGenEventSubscriber_mi);
+
+            // PcObject
+            MethodInfo callPcObjEventSubscriber_mi = bridgeType.GetMethod("CallPcObjectEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
+            CallPcObjEventSubsCache = new DelegateCache<CallPcObjEventSubscriberDelegate>(callPcObjEventSubscriber_mi);
+
+            // PcStandard
+            MethodInfo callPcStEventSubscriber_mi = bridgeType.GetMethod("CallPcStEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
+            CallPcStEventSubsCache = new DelegateCache<CallPcStandardEventSubscriberDelegate>(callPcStEventSubscriber_mi);
+
+            // PcChanging
+            MethodInfo callPChangingEventSubscriber_mi = bridgeType.GetMethod("CallPChangingEventSubscriber", BindingFlags.Instance | BindingFlags.NonPublic);
+            CallPChangingEventSubsCache = new DelegateCache<CallPChangingEventSubscriberDelegate>(callPChangingEventSubscriber_mi);
+
+            DelegateProxyCache = new DelegateProxyCache();
+        }
+    }
+}
