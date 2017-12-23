5c438d6617694f97cbd2a576f584d8e99384d3b1
drminor <david.minor@live.com>
2017-12-23 01:10:06 -0500

    Trivial update: removed unused type aliases.

--- TypeSafePropertyBag/PropStore/PropBagProxy.cs
+++ TypeSafePropertyBag/PropStore/PropBagProxy.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 
-
 namespace DRM.TypeSafePropertyBag
 {
-    #region Type Aliases
-    using CompositeKeyType = UInt64;
-    using ObjectIdType = UInt64;
-    using PropIdType = UInt32;
-    using PropNameType = String;
-
-    using L2KeyManType = IL2KeyMan<UInt32, String>;
-    #endregion
-
     public class PropBagProxy : IPropBagProxy
     {
         #region Public Properties
