87397432d034e9c91deb62f2194aca1c98f899aa
drminor <david.minor@live.com>
2017-12-23 00:51:56 -0500

    Trivial update: removed some commented-out code.

--- TypeSafePropertyBag/LocalBinding/Registration/IRegisterBindings.cs
+++ TypeSafePropertyBag/LocalBinding/Registration/IRegisterBindings.cs
@@ -8,9 +8,6 @@ namespace DRM.TypeSafePropertyBag
         bool RegisterBinding<T>(IPropBag targetPropBag, L2T propId, LocalBindingInfo bindingInfo);
 
         bool UnregisterBinding<T>(IPropBag targetPropBag, L2T propId, LocalBindingInfo bindingInfo);
-
-        //bool RegisterBinding(IPropBag targetPropBag, L2T propId, LocalBindingInfo bindingInfo);
-
-        //bool UnRegisterBinding(IPropBag targetPropBag, L2T propId, LocalBindingInfo bindingInfo);
     }
+
 }
