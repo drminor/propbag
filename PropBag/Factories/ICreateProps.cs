b0f5586958e990d2ea5697c772971f3ffcda7a7d
drminor <david.minor@live.com>
2017-12-23 20:00:08 -0500

    Continue to work on CollectionViewSource support.

--- PropBag/Factories/ICreateProps.cs
+++ PropBag/Factories/ICreateProps.cs
@@ -9,10 +9,13 @@ namespace DRM.PropBag
         ICProp<CT, T> CreateCPropFromObject<CT, T>(IPropFactory propFactory, object value, string propertyName, object extraInfo, bool hasStorage, bool isTypeSolid, Delegate comparer, bool useRefEquality = false) where CT : class, IObsCollection<T>;
         ICProp<CT, T> CreateCPropFromString<CT, T>(IPropFactory propFactory, string value, bool useDefault, string propertyName, object extraInfo, bool hasStorage, bool isTypeSolid, Delegate comparer, bool useRefEquality = true) where CT : class, IObsCollection<T>;
         ICProp<CT, T> CreateCPropWithNoValue<CT, T>(IPropFactory propFactory, bool useDefault, string propertyName, object extraInfo, bool hasStorage, bool isTypeSolid, Delegate comparer, bool useRefEquality = true) where CT : class, IObsCollection<T>;
+
         T CreateNewUsingDefaultConstructor<T>();
+
         IProp<T> CreatePropFromObject<T>(IPropFactory propFactory, object value, string propertyName, object extraInfo, bool hasStorage, bool isTypeSolid, Delegate comparer, bool useRefEquality = false);
         IProp<T> CreatePropFromString<T>(IPropFactory propFactory, string value, bool useDefault, string propertyName, object extraInfo, bool hasStorage, bool isTypeSolid, Delegate comparer, bool useRefEquality = false);
         IProp<T> CreatePropWithNoValue<T>(IPropFactory propFactory, string propertyName, object extraInfo, bool hasStorage, bool isTypeSolid, Delegate comparer, bool useRefEquality = false);
+
         Func<CT, CT, bool> GetComparerForCollections<CT>(Delegate comparer, IPropFactory propFactory, bool useRefEquality);
         Func<T, T, bool> GetComparerForProps<T>(Delegate comparer, IPropFactory propFactory, bool useRefEquality);
     }
---
+++ TypeSafePropertyBag/PropInterfaces/IProvideADataSourceProvider_NotUsed.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Data;
+
+namespace DRM.TypeSafePropertyBag.Unused
+{
+    public interface IProvideADataSourceProvider
+    {
+        DataSourceProvider DataSourceProvider { get; }
+        bool IsCollection();
+        bool IsReadOnly();
+    }
+}
