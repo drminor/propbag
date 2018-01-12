949358ec6ff01f6307defbcb5b16382459e73586
drminor <david.minor@live.com>
2018-01-12 03:33:34 -0500

    Moved ObsCollection to the Unused folder.

--- TypeSafePropertyBag/Unused/ObsCollection.cs
+++ TypeSafePropertyBag/Unused/ObsCollection.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 
-namespace DRM.TypeSafePropertyBag.Fundamentals
+namespace DRM.TypeSafePropertyBag.Fundamentals.Unused
 {
     public class ObsCollection<T> : IObsCollection<T>
     {
