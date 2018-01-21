ad30c88b7ca0eaa484ef95a6287415dc7a4c737a
drminor <david.minor@live.com>
2018-01-20 23:29:05 -0500

    Moved the CVPropFromDsDelegate to its own code file.

---
+++ TypeSafePropertyBag/CVPropFromDsDelegate.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DRM.TypeSafePropertyBag
+{
+    using PropNameType = String;
+
+    public delegate IProp CVPropFromDsDelegate(IPropBag target, PropNameType propertyName, PropNameType srcPropName, IMapperRequest mr);
+}
