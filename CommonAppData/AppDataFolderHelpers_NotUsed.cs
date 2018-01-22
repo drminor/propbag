dfde5c53ccd8f6c56c26f38a60e0f8f4a7ad9e23
drminor <david.minor@live.com>
2018-01-22 01:04:33 -0500

    Created new project: CommonAppData. This project provides methods to create and access a company/application folder within the Environment.SpecialFolder.CommonApplicationData folder.

---
+++ CommonAppData/AppDataFolderHelpers_NotUsed.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/// <remarks>
+/// Copied from https://www.codeproject.com/Tips/370232/Where-should-I-store-my-data
+/// Author: Original Griff: https://www.codeproject.com/Members/OriginalGriff
+/// License: Code Project Open License (CPOL)
+/// </remarks>>
+
+namespace CommonAppData
+{
+    class AppDataFolderHelpers
+    {
+        /// <summary>
+        /// Get the Application Guid
+        /// </summary>
+        public static Guid AppGuid
+        {
+            get
+            {
+                Assembly asm = Assembly.GetEntryAssembly();
+                object[] attr = (asm.GetCustomAttributes(typeof(GuidAttribute), true));
+                return new Guid((attr[0] as GuidAttribute).Value);
+            }
+        }
+
+        /// <summary>
+        /// Get the current assembly Guid.
+        /// <remarks>
+        /// Note that the Assembly Guid is not necessarily the same as the
+        /// Application Guid - if this code is in a DLL, the Assembly Guid
+        /// will be the Guid for the DLL, not the active EXE file.
+        /// </remarks>
+        /// </summary>
+        public static Guid AssemblyGuid
+        {
+            get
+            {
+                Assembly asm = Assembly.GetExecutingAssembly();
+                object[] attr = (asm.GetCustomAttributes(typeof(GuidAttribute), true));
+                return new Guid((attr[0] as GuidAttribute).Value);
+            }
+        }
+
+        /// <summary>
+        /// Get the current user data folder
+        /// </summary>
+        /// 
+        public static string UserDataFolder
+        {
+            get
+            {
+                Guid appGuid = AppGuid;
+                string folderBase = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return GetFolder(folderBase, appGuid.ToString("B").ToUpper());
+            }
+        }
+
+        public string GetUserDataFolder(string subFolder)
+        {
+            string folderBase = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            return GetFolder(folderBase, subFolder);
+        }
+
+        /// <summary>
+        /// Get the current user roaming data folder
+        /// </summary>
+        public static string UserRoamingDataFolder
+        {
+            get
+            {
+                Guid appGuid = AppGuid;
+                string folderBase = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                return GetFolder(folderBase, appGuid.ToString("B").ToUpper());
+            }
+        }
+
+        public string GetUserRoamingDataFolder(string subFolder)
+        {
+            string folderBase = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            return GetFolder(folderBase, subFolder);
+        }
+
+        /// <summary>
+        /// Get all users data folder
+        /// </summary>
+        public static string AllUsersDataFolder
+        {
+            get
+            {
+                Guid appGuid = AppGuid;
+                string folderBase = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+                return GetFolder(folderBase, appGuid.ToString("B").ToUpper());
+            }
+        }
+
+        public string GetAllUsersDataFolder(string subFolder)
+        {
+            string folderBase = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            return GetFolder(folderBase, subFolder);
+        }
+
+        private static string GetFolder(string folderBase, string subFolder)
+        {
+            string dir = string.Format(@"{0}\{1}\", folderBase, subFolder);
+            return CheckDir(dir);
+        }
+
+        /// <summary>
+        /// Check the specified folder, and create if it doesn't exist.
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <returns></returns>
+        private static string CheckDir(string dir)
+        {
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            return dir;
+        }
+    }
+}
---
+++ CommonAppData/DBActivator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data.Entity;
+using System.Reflection;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CommonAppData
+{
+    public interface IHaveADbContext
+    {
+        DbContext DbContext { get; }
+    }
+
+    public class DBActivator<T> : IHaveADbContext where T: DbContext, new()
+    {
+        public T TypedDbContext { get; }
+
+        public DbContext DbContext => TypedDbContext;
+
+        public DBActivator()
+        {
+            TypedDbContext = GetNewDbContext();
+        }
+
+        public DBActivator(string dataDirPath)
+        {
+            SetDataDir(dataDirPath);
+            TypedDbContext = GetNewDbContext();
+        }
+
+        public DBActivator(CommonApplicationData commonApplicationData)
+        {
+            SetDataDir(commonApplicationData.ApplicationFolderPath);
+            TypedDbContext = GetNewDbContext();
+        }
+
+        public DBActivator(Environment.SpecialFolder specialFolder)
+        {
+            string dataDirPath = Environment.GetFolderPath(specialFolder);
+            SetDataDir(dataDirPath);
+            TypedDbContext = GetNewDbContext();
+        }
+
+        public DBActivator(Environment.SpecialFolder specialFolder, string subFolder)
+        {
+            string dataDirPath = Environment.GetFolderPath(specialFolder);
+            dataDirPath = Path.Combine(dataDirPath, subFolder);
+
+            SetDataDir(dataDirPath);
+            TypedDbContext = GetNewDbContext();
+        }
+
+        public DBActivator(Environment.SpecialFolder specialFolder, string companyFolder, string appFolder)
+        {
+            string dataDirPath = Environment.GetFolderPath(specialFolder);
+            dataDirPath = Path.Combine(dataDirPath, companyFolder, appFolder);
+            SetDataDir(dataDirPath);
+            TypedDbContext = GetNewDbContext();
+        }
+
+        public DBActivator(Assembly assembly)
+        {
+            string companyName = GetAssemblyCompany(assembly);
+            string assemblyId = GetAssemblyId(assembly);
+
+            CommonApplicationData cad = new CommonApplicationData(companyName, assemblyId, allUsers: false);
+            SetDataDir(cad.ApplicationFolderPath);
+            TypedDbContext = GetNewDbContext();
+        }
+
+        private string GetAssemblyId(Assembly assembly)
+        {
+            object[] attr = (assembly.GetCustomAttributes(typeof(GuidAttribute), true));
+            Guid guid = new Guid((attr[0] as GuidAttribute).Value);
+            string result = guid.ToString("B").ToUpper();
+            return result;
+        }
+
+        private string GetAssemblyCompany(Assembly assembly)
+        {
+            object[] attr = (assembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute), true));
+            string result = ((AssemblyCompanyAttribute)attr[0]).Company;
+            return result;
+        }
+
+        private void SetDataDir(string dataDirPath)
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", dataDirPath);
+        }
+
+        private T GetNewDbContext()
+        {
+            T result = new T();
+            return result;
+        }
+    }
+}
