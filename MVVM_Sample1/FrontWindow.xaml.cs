c10ee7ba602017e0df3acb4f7513ff220d6916d4
drminor <david.minor@live.com>
2018-02-10 18:10:39 -0500

    Added calls to MemConsumptionTracker.

--- MVVM_Sample1/FrontWindow.xaml.cs
+++ MVVM_Sample1/FrontWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CommonAppData;
 using MVVMApplication.View;
+using ObjectSizeDiagnostics;
 using System;
 using System.Reflection;
 using System.Windows;
@@ -8,27 +9,59 @@ namespace MVVMApplication
 {
     public partial class FrontWindow : Window
     {
+        #region Private Members
+
+        MemConsumptionTracker _mct = new MemConsumptionTracker(enabled: false);
+
+        #endregion
+
+        #region Constructor
+
         public FrontWindow()
         {
-            InitializeComponent();
             SetDataDirLocation();
+            InitializeComponent();
         }
+
+        #endregion
+
+        #region Event Handlers
 
         private void Plain_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show(this, "This is here to run the application using 'standard' MVVM WPF technology" +
+                " as a base-line comparison, however currently the 'plain' MainWindow is not working.",
+                "Feature Not Available", MessageBoxButton.OK, MessageBoxImage.Information);
+
             //MainWindowPlain mwp = new MainWindowPlain();
             //mwp.ShowDialog();
         }
 
         private void PropBagProxyEmit_Click(object sender, RoutedEventArgs e)
         {
+            _mct.Enabled = true;
+            _mct.Compact("Before Starting MainWindow (EmitProxy)");
+
             ShowMain("Emit_Proxy");
+
+            _mct.MeasureAndReport("The Main Window has been closed. Before Compaction.", "FrontWindow");
+            _mct.Compact();
+            _mct.MeasureAndReport("The Main Window has been closed. After Compaction.", "FrontWindow");
         }
 
         private void PropBagExtraMembers_Click(object sender, RoutedEventArgs e)
         {
             ShowMain("Extra_Members");
         }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Private Methods
 
         private void ShowMain(string configPackageNameSuffix)
         {
@@ -43,9 +76,6 @@ namespace MVVMApplication
             AppDomain.CurrentDomain.SetData("DataDirectory", cad.ApplicationFolderPath);
         }
 
-        private void Window_Closed(object sender, EventArgs e)
-        {
-
-        }
+        #endregion
     }
 }
