23c7a431002b35c1f440a9eba2ab7e381d340579
drminor <david.minor@live.com>
2018-02-18 01:43:22 -0500

    Created new interface: IMappedDSP.

---
+++ TypeSafePropertyBag/DataAccessSupport/IMappedDSP.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DRM.TypeSafePropertyBag.DataAccessSupport
+{
+    public interface IMappedDSP<T> : IDisposable, INotifyItemEndEdit, IProvideADataSourceProvider, IHaveACrudWithMapping<T> where T : INotifyItemEndEdit
+    {
+    }
+}
