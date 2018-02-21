1147df0a041d3a04b04abf0987c071aa7ea5a638
drminor <david.minor@live.com>
2018-02-21 00:36:00 -0500

    Renamed interface: ICacheLevel2KeyManagers to ICachePropItemSets.

---
+++ TypeSafePropertyBag/PropStore/ICachePropItemSets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DRM.TypeSafePropertyBag
+{
+    using GenerationIdType = Int64;
+
+    internal interface ICachePropItemSets<PropItemSetType, L2T, L2TRaw> where PropItemSetType : class, IPropNodeCollection_Internal<L2T, L2TRaw>
+    {
+        bool TryGetValueAndGenerationId(PropItemSetType propItemSet, out PropItemSetType basePropItemSet, out GenerationIdType generationId);
+
+        bool TryGetGeneration(PropItemSetType propItemSet, out GenerationIdType generationId);
+
+        bool TryRegisterBasePropItemSet(PropItemSetType propItemSet);
+
+        bool TryRegisterPropItemSet(PropItemSetType propItemSet, PropItemSetType basePropItemSet, out GenerationIdType generationId);
+    }
+}
