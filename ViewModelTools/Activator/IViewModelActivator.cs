c5aba3a915f1c802f09b09470a76917d656d0ebd
drminor <david.minor@live.com>
2018-02-27 00:27:48 -0500

    Reorded the paremeters for method GetNewViewModel on interface: IViewModelActivator

--- ViewModelTools/Activator/IViewModelActivator.cs
+++ ViewModelTools/Activator/IViewModelActivator.cs
@@ -9,20 +9,20 @@ namespace DRM.PropBag.ViewModelTools
     public interface IViewModelActivator
     {
         // Create new Type that is derived from a Type known only at run time.
-        object GetNewViewModel(IPropModel propModel, PSAccessServiceCreatorInterface storeAccessCreator,
-            Type typeToCreate, IPropFactory propFactory = null, string fullClassName = null);
+        object GetNewViewModel(Type typeToCreate, IPropModel propModel,
+            PSAccessServiceCreatorInterface storeAccessCreator, IPropFactory propFactory, string fullClassName);
 
         // With AutoMapper Support
-        object GetNewViewModel(IPropModel propModel, PSAccessServiceCreatorInterface storeAccessCreator,
-            Type typeToCreate, IProvideAutoMappers autoMapperService, IPropFactory propFactory, string fullClassName);
+        object GetNewViewModel(Type typeToCreate, IPropModel propModel,
+            PSAccessServiceCreatorInterface storeAccessCreator, IProvideAutoMappers autoMapperService, IPropFactory propFactory, string fullClassName);
 
         // Create new Type that is derived from a Type known at compile time.
         object GetNewViewModel<BT>(IPropModel propModel, PSAccessServiceCreatorInterface storeAccessCreator,
-            IPropFactory propFactory = null, string fullClassName = null) where BT : class, IPropBag;
+            IPropFactory propFactory, string fullClassName) where BT : class, IPropBag;
 
         // With AutoMapper Support
         object GetNewViewModel<BT>(IPropModel propModel, PSAccessServiceCreatorInterface storeAccessCreator,
-            IProvideAutoMappers autoMapperService, IPropFactory propFactory = null, string fullClassName = null) where BT : class, IPropBag;
+            IProvideAutoMappers autoMapperService, IPropFactory propFactory, string fullClassName) where BT : class, IPropBag;
 
         object GetNewViewModel(Type typeToCreate, IPropBag copySource);
         object GetNewViewModel<BT>(IPropBag copySource) where BT : class, IPropBag;
