ed1bfe026e2b8082e2e2c4fc22b8af219247e2c1
drminor <david.minor@live.com>
2018-02-27 20:50:57 -0500

    Create new class: ActivationInfo to be used in the IPropTemplate. This is not used at the moment and may never be used.

---
+++ TypeSafePropertyBag/Fundamentals/ActivationInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DRM.TypeSafePropertyBag.Fundamentals
+{
+    public class ActivationInfo
+    {
+        public ActivationInfo(Type type, IList<Argument> arguments)
+        {
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+            Arguments = arguments ?? throw new ArgumentNullException(nameof(arguments));
+        }
+
+        public Type Type { get; }
+        public IList<Argument> Arguments { get; }
+    }
+
+    public class Argument
+    {
+        public Argument(Type type, int position) : this(type, position, null)
+        {
+        }
+
+        public Argument(Type type, string name) : this(type, name, null)
+        {
+        }
+
+        public Argument(Type type, int position, object value)
+        {
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+            Position = position;
+            Name = null;
+            Value = value;
+        }
+
+        public Argument(Type type, string name, object value)
+        {
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+            Position = -1;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Value = value;
+        }
+
+        public Type Type { get; }
+        public int Position { get; }
+        public string Name { get; }
+        public object Value { get; }
+    }
+}
