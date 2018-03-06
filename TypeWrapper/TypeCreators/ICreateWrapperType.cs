f4935cfbf5bb95fbaa2c91a24a8dc21862bdd746
drminor <david.minor@live.com>
2018-03-06 16:54:45 -0500

    Moved the ICreateWrapperType interface and the SimpleWrapperTypeCreator implementation from the ViewModelTools project to the TypeWrapper project.

--- TypeWrapper/TypeCreators/ICreateWrapperType.cs
+++ TypeWrapper/TypeCreators/ICreateWrapperType.cs
@@ -1,21 +1,15 @@
 ﻿using DRM.TypeSafePropertyBag;
 using System;
 
-namespace DRM.PropBag.ViewModelTools
+namespace DRM.PropBag.TypeWrapper
 {
     using PropNameType = String;
     using PropModelType = IPropModel<String>;
 
     public interface ICreateWrapperTypes
     {
-        // This could easily be re-instated.
-        //Type GetWrapperType(string resourceKey, Type typeToCreate);
         Type GetWrapperType(PropModelType propModel, Type typeToCreate);
-
-        // This could easily be re-instated.
-        //Type GetWrapperType<BT>(string resourceKey) where BT : class, IPropBag;
         Type GetWrapperType<BT>(PropModelType propModel) where BT : class, IPropBag;
-
         long ClearTypeCache();
     }
 }
---
+++ TypeWrapper/TypeCreators/SimpleWrapperTypeCreator.cs
@@ -0,0 +1,75 @@
+﻿using DRM.TypeSafePropertyBag;
+using DRM.TypeSafePropertyBag.Fundamentals;
+using System;
+
+namespace DRM.PropBag.TypeWrapper
+{
+    using PropNameType = String;
+    using PropModelType = IPropModel<String>;
+
+    public class SimpleWrapperTypeCreator : ICreateWrapperTypes
+    {
+        #region Private Members
+
+        private ICacheWrapperTypes _wrapperTypeCachingService { get; }
+        ICacheTypeDescriptions _typeDescCachingService { get; }
+
+        #endregion
+
+        #region Constructor 
+
+        public SimpleWrapperTypeCreator(
+            ICacheWrapperTypes wrapperTypeCachingService,
+            ICacheTypeDescriptions typeDescCachingService)
+        {
+            _wrapperTypeCachingService = wrapperTypeCachingService ?? throw new ArgumentNullException(nameof(wrapperTypeCachingService));
+            _typeDescCachingService = typeDescCachingService ?? throw new ArgumentNullException(nameof(typeDescCachingService));
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public Type GetWrapperType(PropModelType propModel, Type typeToCreate)
+        {
+            if (!typeToCreate.IsPropBagBased())
+            {
+                throw new InvalidOperationException($"Type: {typeToCreate.Name} must derive from IPropBag.");
+            }
+
+            //TypeDescription td = _typeDescCachingService.GetOrAdd(new NewTypeRequest(propModel, typeToCreate, null));
+            //Type newWrapperType = _wrapperTypeCachingService.GetOrAdd(td);
+            //return newWrapperType;
+
+            Type result = GetWrapperType_Internal(propModel, typeToCreate, null);
+            return result;
+        }
+
+        public Type GetWrapperType<BT>(PropModelType propModel) where BT : class, IPropBag
+        {
+            //TypeDescription td = _typeDescCachingService.GetOrAdd(new NewTypeRequest(propModel, typeof(BT), null));
+            //Type newWrapperType = _wrapperTypeCachingService.GetOrAdd(td);
+            //return newWrapperType;
+
+            Type result = GetWrapperType_Internal(propModel, typeof(BT), null);
+            return result;
+        }
+
+        private Type GetWrapperType_Internal(PropModelType propModel, Type typeToCreate, string fullClassName)
+        {
+            TypeDescription td = _typeDescCachingService.GetOrAdd(new NewTypeRequest(propModel, typeToCreate, fullClassName));
+            Type newWrapperType = _wrapperTypeCachingService.GetOrAdd(td);
+
+            return newWrapperType;
+        }
+
+        // TODO: Note: This class hold two caches, however only one contributes to the result.
+        public long ClearTypeCache()
+        {
+           long numTypeDescriptionsThatWereCached = _typeDescCachingService.ClearTypeCache();
+           return _wrapperTypeCachingService.ClearTypeCache();
+        }
+
+        #endregion
+    }
+}
