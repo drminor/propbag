650d4d5fe95fb56093c13b1b3a7e1661dad2cb77
drminor <david.minor@live.com>
2018-03-06 16:58:46 -0500

    Moved the BetterListCollectionView class to the PropBagWPF project and removed the dependency on PresentationFramework from the TypeSafePropBag project.

---
+++ PropBagWPF/BetterListCollectionView.cs
@@ -0,0 +1,64 @@
+﻿using DRM.TypeSafePropertyBag;
+using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+
+/// <remarks>
+/// Copied from http://pelebyte.net/blog/2009/10/01/collections-collectionviews-and-a-wpf-binding-memory-leak/
+/// </remarks>
+
+namespace DRM.PropBagWPF
+{
+    public class BetterListCollectionViewCreator<T> where T: INotifyItemEndEdit
+    {
+        public static ICollectionView ProduceView(ObservableCollection<T> list)
+        {
+            ICollectionView result = new BetterListCollectionView<T>(list);
+
+            return result;
+        }
+    }
+
+
+    public class BetterListCollectionView<T> : ListCollectionView, IWeakEventListener
+    {
+        public BetterListCollectionView(ObservableCollection<T> list) : base(list)
+        {
+            //if (list is INotifyCollectionChanged changed)
+            //{
+            //    this fixes the problem
+            //    changed.CollectionChanged -= this.OnCollectionChanged;
+            //    CollectionChangedEventManager.AddListener(changed, this);
+            //}
+
+            list.CollectionChanged -= this.OnCollectionChanged;
+            CollectionChangedEventManager.AddListener(list, this);
+        }
+
+        public bool ReceiveWeakEvent(Type managerType, object sender, EventArgs e)
+        {
+            if (e is NotifyCollectionChangedEventArgs nccEventArgs)
+            {
+                OnCollectionChanged(nccEventArgs);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    //public class BetterObservableCollection_NotUsed<T> : ObservableCollection<T>, ICollectionViewFactory
+    //{
+    //    public ICollectionView CreateView()
+    //    {
+    //        return new BetterListCollectionView(this);
+    //    }
+    //}
+}
---
+++ TypeSafePropertyBag/DataAccessSupport/BetterLCVCreatorDelegate.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace DRM.TypeSafePropertyBag.DataAccessSupport
+{
+    public delegate ICollectionView BetterLCVCreatorDelegate<T>(ObservableCollection<T> list) where T: INotifyItemEndEdit;
+}
