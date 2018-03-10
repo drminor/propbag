ff1db442a7b1bcf515b6fd0b5e8a9572771cd144
drminor <david.minor@live.com>
2018-03-09 19:17:08 -0500

    Renamed PropFactoryFactory to SimplePropFactoryFactory.

--- TypeSafePropertyBag/Factories/SimplePropFactoryFactory.cs
+++ TypeSafePropertyBag/Factories/SimplePropFactoryFactory.cs
@@ -2,22 +2,23 @@
 
 namespace DRM.TypeSafePropertyBag
 {
-    public class PropFactoryFactory : IPropFactoryFactory
+    public class SimplePropFactoryFactory : IPropFactoryFactory
     {
         private readonly IProvideDelegateCaches _delegateCacheProvider;
         private readonly IConvertValues _valueConverter;
         private readonly ResolveTypeDelegate _typeResolver;
 
-        private PropFactoryFactory()
+        private SimplePropFactoryFactory()
         {
+            throw new NotSupportedException($"Use of the parameterless constructor to create a {nameof(SimplePropFactoryFactory)} is not supported.");
         }
 
-        public PropFactoryFactory(IProvideDelegateCaches delegateCacheProvider, IConvertValues valueConverter)
+        public SimplePropFactoryFactory(IProvideDelegateCaches delegateCacheProvider, IConvertValues valueConverter)
             : this(delegateCacheProvider, valueConverter, null)
         {
         }
 
-        public PropFactoryFactory(IProvideDelegateCaches delegateCacheProvider, IConvertValues valueConverter, ResolveTypeDelegate typeResolver)
+        public SimplePropFactoryFactory(IProvideDelegateCaches delegateCacheProvider, IConvertValues valueConverter, ResolveTypeDelegate typeResolver)
         {
             _delegateCacheProvider = delegateCacheProvider;
             _valueConverter = valueConverter;
