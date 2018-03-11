5c98848522521dd67a7a6b784953225315e11bda
drminor <david.minor@live.com>
2018-03-11 17:13:57 -0400

    Trivial update: reformatted code to keep open braces near left margin.

--- TypeSafePropertyBag/Unused/HyperTypeDescriptors/HyperTypeDescriptionProvider.cs
+++ TypeSafePropertyBag/Unused/HyperTypeDescriptors/HyperTypeDescriptionProvider.cs
@@ -10,30 +10,56 @@ using System.Security.Permissions;
  *                                  (thanks/credit to Josh Smith for feedback/hints)
  */
 
-namespace Hyper.ComponentModel {
-    public sealed class HyperTypeDescriptionProvider : TypeDescriptionProvider {
-        public static void Add(Type type) {
+namespace Hyper.ComponentModel
+{
+    public sealed class HyperTypeDescriptionProvider : TypeDescriptionProvider
+    {
+        public static void Add(Type type)
+        {
             TypeDescriptionProvider parent = TypeDescriptor.GetProvider(type);
             TypeDescriptor.AddProvider(new HyperTypeDescriptionProvider(parent), type);
         }
-        public HyperTypeDescriptionProvider() : this(typeof(object)) { }
-        public HyperTypeDescriptionProvider(Type type) : this(TypeDescriptor.GetProvider(type)) { }
-        public HyperTypeDescriptionProvider(TypeDescriptionProvider parent) : base(parent) { }
-        public static void Clear(Type type) {
-            lock (descriptors) {
+
+        public HyperTypeDescriptionProvider()
+            : this(typeof(object))
+        {
+        }
+
+        public HyperTypeDescriptionProvider(Type type)
+            : this(TypeDescriptor.GetProvider(type))
+        {
+        }
+
+        public HyperTypeDescriptionProvider(TypeDescriptionProvider parent)
+            : base(parent)
+        {
+        }
+
+        public static void Clear(Type type)
+        {
+            lock (descriptors)
+            {
                 descriptors.Remove(type);
             }
         }
-        public static void Clear() {
-            lock (descriptors) {
+
+        public static void Clear()
+        {
+            lock (descriptors)
+            {
                 descriptors.Clear();
             }
         }
+
         private static readonly Dictionary<Type, ICustomTypeDescriptor> descriptors = new Dictionary<Type, ICustomTypeDescriptor>();
-        public sealed override ICustomTypeDescriptor GetTypeDescriptor(Type objectType, object instance) {
+
+        public sealed override ICustomTypeDescriptor GetTypeDescriptor(Type objectType, object instance)
+        {
             ICustomTypeDescriptor descriptor;
-            lock (descriptors) {
-                if (!descriptors.TryGetValue(objectType, out descriptor)) {
+            lock (descriptors)
+            {
+                if (!descriptors.TryGetValue(objectType, out descriptor))
+                {
                     try
                     {
                         descriptor = BuildDescriptor(objectType);
@@ -46,6 +72,7 @@ namespace Hyper.ComponentModel {
                 return descriptor;
             }
         }
+
         [ReflectionPermission( SecurityAction.Assert, Flags = ReflectionPermissionFlag.AllFlags)]
         private ICustomTypeDescriptor BuildDescriptor(Type objectType)
         {
