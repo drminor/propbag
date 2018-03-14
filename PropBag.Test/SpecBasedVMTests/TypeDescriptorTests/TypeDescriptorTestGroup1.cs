d282b15e57ce0a69b5cc3cb6366e099b03c5cb95
drminor <david.minor@live.com>
2018-03-14 09:14:51 -0400

    The Test: CreateDestroyBasicVM_PersonVM_HasPropDescriptors now passes.

--- PropBag.Test/SpecBasedVMTests/TypeDescriptorTests/TypeDescriptorTestGroup1.cs
+++ PropBag.Test/SpecBasedVMTests/TypeDescriptorTests/TypeDescriptorTestGroup1.cs
@@ -1,30 +1,29 @@
 ﻿using DRM.TypeSafePropertyBag;
+using DRM.TypeSafePropertyBag.Fundamentals;
 using NUnit.Framework;
-using PropBagLib.Tests.BusinessModel;
 using PropBagLib.Tests.SpecBasedVMTests.BasicVM.Services;
 using PropBagLib.Tests.SpecBasedVMTests.BasicVM.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel;
 
 namespace PropBagLib.Tests.SpecBasedVMTests.TypeDescriptorTests
 {
-    using PropNameType = String;
     using PropModelType = IPropModel<String>;
 
-    [TestFixture(TestName = "CreateDestroyCreateBasicVM")]
+    [TestFixture(TestName = "CreateDestroyBasicVM")]
     [NonParallelizable]
-    public class CreateDestroyCreateBasicVM : TypeDescriptorSetup
+    public class CreateDestroyBasicVM : TypeDescriptorSetup
     {
         PropModelType mainWindowPropModel;
         MainWindowViewModel mainWindowViewModel;
+        //MainWindowViewModel mainWindowViewModel2;
+
 
         protected override Action EstablishContext()
         {
-            // Take Heap SnapShot here. (CreateVM_CreateMainWindowVM_Run1 has completed and the Context has been cleaned up.)
-
-            //ConfigPackageNameSuffix = "Emit_Proxy";
-            ConfigPackageNameSuffix = "Extra_Members";
+            ConfigPackageNameSuffix = "Emit_Proxy";
+            //ConfigPackageNameSuffix = "Extra_Members";
 
             return base.EstablishContext();
         }
@@ -32,41 +31,64 @@ namespace PropBagLib.Tests.SpecBasedVMTests.TypeDescriptorTests
         protected override void Because_Of()
         {
             mainWindowPropModel = PropModelCache.GetPropModel("MainWindowVM");
-            BaseMemTracker.CompactMeasureAndReport("After get mainWindow_PropModel.", "CreateVM_CreateMainWindowVM_Run2");
+            BaseMemTracker.CompactMeasureAndReport("After get mainWindow_PropModel.", "CreateDestroyBasicVM");
 
             // To see how much memory is not being cleaned up after one is created and then disposed.
             mainWindowViewModel = new MainWindowViewModel(mainWindowPropModel, PropStoreAccessService_Factory, AutoMapperProvider, propFactory: null, fullClassName: null);
-            BaseMemTracker.CompactMeasureAndReport("After create the first mainWindowViewModel.", "CreateVM_CreateMainWindowVM_Run2");
+            BaseMemTracker.CompactMeasureAndReport("After create the first mainWindowViewModel.", "CreateDestroyBasicVM");
 
-            mainWindowViewModel.Dispose();
-            BaseMemTracker.CompactMeasureAndReport("After dispose of the first mainWindowViewModel.", "CreateVM_CreateMainWindowVM_Run2");
+            //mainWindowViewModel.Dispose();
+            //BaseMemTracker.CompactMeasureAndReport("After dispose of the first mainWindowViewModel.", "CreateDestroyBasicVM");
 
-            mainWindowViewModel = new MainWindowViewModel(mainWindowPropModel, PropStoreAccessService_Factory, AutoMapperProvider, propFactory: null, fullClassName: null);
-            BaseMemTracker.CompactMeasureAndReport("After create the second mainWindowViewModel.", "CreateVM_CreateMainWindowVM_Run2");
+            //mainWindowViewModel2 = new MainWindowViewModel(mainWindowPropModel, PropStoreAccessService_Factory, AutoMapperProvider, propFactory: null, fullClassName: null);
+            //BaseMemTracker.CompactMeasureAndReport("After create the second mainWindowViewModel.", "CreateDestroyBasicVM");
 
-            // And here.
-            mainWindowViewModel.Dispose();
-            BaseMemTracker.CompactMeasureAndReport("After dispose of the second mainWindowViewModel.", "CreateVM_CreateMainWindowVM_Run2");
+            //// And here.
+            //mainWindowViewModel2.Dispose();
+            //BaseMemTracker.CompactMeasureAndReport("After dispose of the second mainWindowViewModel.", "CreateVM_CreateMainWindowVM_Run2");
         }
 
         [Test]
-        public void CreateVM2_ThenGetPropModel()
+        public void CreateDestroyBasicVM_ThenGetPropModel()
         {
             Assert.That(mainWindowPropModel != null, "mainWindowPropModel is null.");
         }
 
         [Test]
-        public void CreateVM2_MainWindowPropModelHasAllTheProps()
+        public void CreateDestroyBasicVM_MainWindowPropModelHasAllTheProps()
         {
             Assert.That(mainWindowPropModel.Count == 4, "mainWindowPropModel does not have 4 PropItems.");
         }
 
         [Test]
-        public void CreateVM2_PersonCollectionViewModel_WasCreated()
+        public void CreateDestroyBasicVM_PersonVM_HasPropDescriptors()
         {
-            //PersonCollectionViewModel personCollectionViewModel = (PersonCollectionViewModel)mainWindowViewModel[typeof(PersonCollectionViewModel), "PersonCollectionVM"];
+            PersonCollectionViewModel personCollectionViewModel = (PersonCollectionViewModel)mainWindowViewModel[typeof(PersonCollectionViewModel), "PersonCollectionVM"];
+            Assert.That(personCollectionViewModel != null, "The personCollectionViewModel could not be found.");
 
-            //Assert.That(personCollectionViewModel != null, "The personCollectionViewModel could not be found.");
+            bool gotTheViewManager = mainWindowViewModel.TryGetViewManager("Business", typeof(PersonDAL), out IManageCViews cViewManager);
+
+            Assert.That(gotTheViewManager, "TryGetViewManager failed for the Business property.");
+
+            Assert.That(cViewManager != null, "The CollectionViewManager for the PersonListView property is null.");
+
+            Type rtt = cViewManager.CollectionItemRunTimeType;
+
+            IList<string> pNamesFromType = TypeInspectorUtility.GetPropertyNames(rtt);
+            IList<string> pNamesFromInstance_WT;
+            IList<string> pNamesFromInstance;
+
+
+            ICollectionView cv = cViewManager.GetDefaultCollectionView();
+
+            if(!cv.IsEmpty)
+            {
+                cv.MoveCurrentToFirst();
+                object x = cv.CurrentItem;
+                pNamesFromInstance_WT = TypeInspectorUtility.GetPropertyNames(rtt, x);
+                pNamesFromInstance = TypeInspectorUtility.GetPropertyNames(x);
+            }
+
         }
 
         [OneTimeSetUp]
@@ -80,6 +102,7 @@ namespace PropBagLib.Tests.SpecBasedVMTests.TypeDescriptorTests
         [OneTimeTearDown]
         public void OneTimeTearDown()
         {
+            mainWindowViewModel.Dispose();
             CallContextDestroyer();
         }
     }
