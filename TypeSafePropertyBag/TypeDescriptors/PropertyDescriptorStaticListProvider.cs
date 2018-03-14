a38f6d75af761a3830c708376d7008701aa2dea6
drminor <david.minor@live.com>
2018-03-14 08:35:06 -0400

    Created new class: PropertyDescriptorStaticListProvider. IPropBag implementation use instances of this class to support the PropBagCustomTypeDescriptor.

---
+++ TypeSafePropertyBag/TypeDescriptors/PropertyDescriptorStaticListProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace DRM.TypeSafePropertyBag
+{
+    public class PropertyDescriptorStaticListProvider
+    {
+        IList<PropertyDescriptor> _list;
+
+        public Func<IList<PropertyDescriptor>> CustomPropsGetter => GetPropertyDescriptors;
+
+        public PropertyDescriptorStaticListProvider(IList<PropertyDescriptor> list)
+        {
+            _list = list;
+        }
+
+        public IList<PropertyDescriptor> GetPropertyDescriptors()
+        {
+            return _list;
+        }
+    }
+}
