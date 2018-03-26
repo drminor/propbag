311070bd134c6e0855450d9ebb24fdd6e0582942
drminor <david.minor@live.com>
2018-03-26 13:37:51 -0400

    Moved the interface: IBuildPropBagMapper from the AutoMapperSupportInterfaces project to the AutoMapperSupport project.

---
+++ AutoMapperSupport/Builders/IBuildPropBagMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DRM.TypeSafePropertyBag;
+using DRM.PropBag.ViewModelTools;
+
+namespace DRM.PropBag.AutoMapperSupport
+{
+    using ViewModelFactoryInterface = IViewModelFactory<UInt32, String>;
+    public interface IBuildPropBagMapper<TSource, TDestination> where TDestination : class, IPropBag
+    {
+        IPropBagMapper<TSource, TDestination> GenerateMapper(IPropBagMapperKey<TSource, TDestination> mapperRequestKey, ViewModelFactoryInterface viewModelFactory);
+
+        Func<IPropBagMapperKeyGen, ViewModelFactoryInterface, IPropBagMapperGen> GenMapperCreator { get; }
+    }
+}
