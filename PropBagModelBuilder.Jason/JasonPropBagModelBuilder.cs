8de978d660d0afbe0053fc72c8515d2c14783e88
drminor <david.minor@live.com>
2018-04-03 22:17:26 -0400

    Renamed project: PropBagModelBuilderJason to PropBagModelBuilder.Jason

---
+++ PropBagModelBuilder.Jason/JasonPropBagModelBuilder.cs
@@ -0,0 +1,36 @@
+﻿using DRM.TypeSafePropertyBag;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swhp.PropBagModelBuilder.Jason
+{
+    public class JasonPropModelBuilder : IPropModelBuilder
+    {
+        public JasonPropModelBuilder
+            (
+            IParsePropBagTemplates propBagTemplateParser,
+            IPropFactoryFactory propFactoryFactory
+            )
+        {
+
+        }
+
+        public IMapperRequest GetMapperRequest(string resourceKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPropModel<string> GetPropModel(string resourceKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, string> GetTypeToKeyMap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
